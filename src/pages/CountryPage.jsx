8f7e65a23dc7faf0a6836080bf88631159c65626
Mathes07-s <thesteeldev@gmail.com>
2026-05-14 14:36:07 +0530

    feat: scaffold CountryPeek with routing, Header, Home shell, and 404 page

---
+++ src/pages/CountryPage.jsx
@@ -0,0 +1,5 @@
+function CountryPage() {
+  return <div className="home">Country detail will appear here.</div>;
+}
+
+export default CountryPage;
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,5 @@
+function Favourites() {
+  return <div className="home">Your favourite countries will appear here.</div>;
+}
+
+export default Favourites;
